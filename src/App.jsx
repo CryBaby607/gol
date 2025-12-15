739da605f79b989c19476555b5decce326c0ed19
CryBaby607 <dortmund607@gmail.com>
2025-12-15 15:30:37 -0600

    V1.2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage'; // <-- Nueva importación
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} /> {/* <-- Nueva ruta */}
       </Routes>
     </div>
   );
